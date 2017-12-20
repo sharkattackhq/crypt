bf6de8a24c1430731bacf2dd092e570023c3c9e7
Nick <nickturner57@gmail.com>
2017-12-20 09:29:32 +0700

    Initial Commit

---
+++ Crypt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Crypt
+//
+//  Created by Admin on 20/12/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ CryptTests/CryptTests.swift
@@ -0,0 +1,36 @@
+//
+//  CryptTests.swift
+//  CryptTests
+//
+//  Created by Admin on 20/12/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import XCTest
+@testable import Crypt
+
+class CryptTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
